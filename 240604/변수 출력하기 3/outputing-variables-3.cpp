885359f7dffcdeebd19caee016566640d3df69e7
Cho Yunhee <68114427+y0c0y@users.noreply.github.com>
2024-06-04 21:22:23 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240604/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 1;
+    int b = 2;
+    char c = 'C';
+    // 여기에 코드를 작성해주세요.
+    cout << a <<"->"<<b<<"->"<<c;
+    return 0;
+}
