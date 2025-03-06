96a7aa1dd93a5c33a44f8240a1ce402bfcdb0de4
Cho Yunhee <68114427+y0c0y@users.noreply.github.com>
2025-03-07 08:50:49 +0900

    Title: 문자열 범위 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250307/문자열 범위 출력하기/print-string-in-range.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+
+    string s;
+
+    getline(cin, s);
+
+    for(auto i =2;i <10;i++)
+    {
+        cout << s[i]; 
+    }
+
+    return 0;
+}
