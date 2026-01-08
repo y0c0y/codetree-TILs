3e7c790e72ad0a1d3ef7682971ce0b9e6cf5baf9
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-01-08 23:46:50 +0900

    Title: 정수의 조건 여부 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/정수의 조건 여부 2/numbers-condition-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a;
+
+    cin >> a;
+
+    if(a==5)
+    {
+        cout << 'A';
+    }
+    else if( a %2 == 0)
+    {
+        cout << 'B';
+    }
+    
+    return 0;
+}
