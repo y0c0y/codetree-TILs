2cd317a7fc826afd86d601910a47f15bc46d5d41
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-01-16 20:50:46 +0900

    Title: 문자열 전부 붙이기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260116/문자열 전부 붙이기/paste-all-string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    string str;
+    string result = "";
+
+    for(int i = 0; i < n; i++)
+    {
+        cin << str;
+        result+= str;
+    }
+
+    cout << result;
+    return 0;
+}
