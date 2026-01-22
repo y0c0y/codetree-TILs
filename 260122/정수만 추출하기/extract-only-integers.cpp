34afe673fd0620db8692eff836e1a56d908b7d40
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-01-22 14:49:19 +0900

    Title: 정수만 추출하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260122/정수만 추출하기/extract-only-integers.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    
+    string str;
+    
+
+    int result = 0;
+
+    int len= 0;
+
+    for(int c = 0; c < 2; c++)
+    {
+        cin >> str;
+
+        len = str.length();
+
+        for(int i = 0; i < len; i++)
+        {
+            if(str[i] < '0' || str[i] > '9') 
+            {
+                string tmp = str.substr(0, i);
+
+                result += stoi(tmp);
+                break;
+            }
+        }
+
+    }
+
+    cout << result;
+
+    
+    return 0;
+}
