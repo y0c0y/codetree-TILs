245de7fcf05e0224e1ed734b5bebe8b745fe8986
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-01-26 13:44:09 +0900

    Title: 비오는 날, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260126/비오는 날/rainy-day.cpp
+++ 260126/비오는 날/rainy-day.cpp
@@ -7,27 +7,7 @@ string date_str;
 string day_str;
 string weather_str;
 
-enum Week
-{
-    Sun,
-    Mon,
-    Tue,
-    Wed,
-    Thu,
-    Fri,
-    Sat
-}
-
-Week WeekStrToInt(string d)
-{
-
-    if(d == "Sun") return Week.Sun;
-    if(d == "Sun") return Week.Sun;
-    if(d == "Sun") return Week.Sun;
-
-}
-
-bool compare(tuple<stirng,string,string> a, tuple<stirng,string,string> b)
+bool compare(tuple<string,string,string> a, tuple<string,string,string> b)
 {
 
     string date_a, day_a, weather_a;
@@ -36,23 +16,33 @@ bool compare(tuple<stirng,string,string> a, tuple<stirng,string,string> b)
     tie(date_a, day_a, weather_a) = a;
     tie(date_b, day_b, weather_b) = b;
 
-    if(date_a != date_b) return a < b;
-    if()
+    return a < b;
 
 }
 
 int main() {
     cin >> n;
-    vector<tuple<stirng,string,string>> arr;
+    vector<tuple<string,string,string>> arr;
 
     for (int i = 0; i < n; i++) {
         cin >> date_str >> day_str >> weather_str;
-        tuple<stirng,string,string> tmp = make_tuple(date_str, day_str, weather_str);
+        tuple<string,string,string> tmp = make_tuple(date_str, day_str, weather_str);
 
         arr.push_back(tmp);
     }
 
     sort(arr.begin(), arr.end(), compare);
+
+    for(auto i : arr)
+    {
+        tie(date_str, day_str, weather_str) = i;
+
+        if(weather_str == "Rain")
+        {
+            cout << date_str << ' ' << day_str << ' '<< weather_str << '\n';
+            break;
+        }
+    }
 
 
     // Please write your code here.
