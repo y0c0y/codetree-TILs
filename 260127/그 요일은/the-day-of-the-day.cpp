073daad5697832cd07e1b01420da517e8789bd28
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-01-27 17:46:09 +0900

    Title: 그 요일은, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260127/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int m1, d1, m2, d2;
+
+int numOfDays[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+string weekStr[7] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+
+int main() {
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    // Please write your code here.
+
+    int total_m1 = 0;
+    int total_m2 = 0;
+
+
+    for(int i = 0; i < m1; i++)
+    {
+        total_m1 += numOfDays[i];
+    }
+
+    total_m1 += d1;
+
+
+    for(int i = 0; i < m2; i++)
+    {
+        total_m2 += numOfDays[i];
+    }
+
+    total_m2 += d2;
+
+
+    int tmp = total_m2 - total_m1;
+
+    cout<<tmp/7+1;
+    return 0;
+}
