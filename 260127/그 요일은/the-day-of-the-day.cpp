8919c9b63e9928f7a736a0e1e10607bd684fb78b
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-01-27 18:00:09 +0900

    Title: 그 요일은, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260127/그 요일은/the-day-of-the-day.cpp
+++ 260127/그 요일은/the-day-of-the-day.cpp
@@ -4,25 +4,34 @@ using namespace std;
 
 int m1, d1, m2, d2;
 
-int numOfDays[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+int numOfDays[13] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
 
 string weekStr[7] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
 
 int main() {
-    cin >> m1 >> d1 >> m2 >> d2;
+
+    string A;
+    cin >> m1 >> d1 >> m2 >> d2 >> A;
 
     // Please write your code here.
 
     int total_m1 = 0;
     int total_m2 = 0;
 
+    int gap = 0;
+
+    for(auto s : weekStr)
+    {
+        if(s == A) break;
+        gap++;
+    }
 
     for(int i = 0; i < m1; i++)
     {
         total_m1 += numOfDays[i];
     }
 
-    total_m1 += d1;
+    total_m1 += d1 + gap;
 
 
     for(int i = 0; i < m2; i++)
@@ -32,9 +41,8 @@ int main() {
 
     total_m2 += d2;
 
+    int tmp = (total_m2 - total_m1) / 7 + 1;
 
-    int tmp = total_m2 - total_m1;
-
-    cout<<tmp/7+1;
+    cout<<tmp;
     return 0;
 }
