7e89e5a5ef22c02f481d7b5cb636f2bd187904d6
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-01-29 01:58:26 +0900

    Title: 흰검 칠하기, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260129/흰검 칠하기/painting-white-black.cpp
+++ 260129/흰검 칠하기/painting-white-black.cpp
@@ -9,87 +9,61 @@ char dir[1000];
 int main() {
     cin >> n;
 
-    map<int,tuple<int,int,char>> arr; // {idx,(white, black)}
+    map<int,tuple<int,int,bool>> arr; // {idx,(white, black)}
 
-    tuple<int,int,char> tmp;
+    tuple<int,int,bool> tmp;
 
     int idx = 0;
     int destination = 0;
     int offset = 0;
 
     int white, black, gray;
-    char ch = '\0';
+    bool isWhite = false;
 
     for (int i = 0; i < n; i++) {
         cin >> x[i] >> dir[i];
 
         if(dir[i] == 'L')
         {
-            destination = idx - x[i];
-            for(; idx > destination; idx--)
-            {
-                // cout << idx << ':';
-                if(arr.end() == arr.find(idx))
-                {
-                    tmp = make_tuple(1,0, 'W');
-                    arr.insert(make_pair(idx, tmp));
-                }
-                else
-                {
-                    tie(white, black, ch) = arr[idx];
-                    tmp = make_tuple(white + 1,black, 'W');
-                    arr[idx] = tmp;
-                }
-
-                // tie(white, black, ch) = arr[idx];
-                // cout << white << ' ' << black << ' '<< ( ch == 'W' ? "White" : "Black" ) << '\n';
-            }
-
-            idx++;
+            offset = -1;
         } 
         else
         {
-            destination = idx + x[i];
-            for(; idx < destination; idx++)
-            {
-                // cout << idx << ':';
-                if(arr.end() == arr.find(idx))
-                {
-                    tmp = make_tuple(0,1, 'B');
-                    arr.insert(make_pair(idx, tmp));
-                }
-                else
-                {
-                    tie(white, black, ch) = arr[idx];
-                    tmp = make_tuple(white,black+1, 'B');
-                    arr[idx] = tmp;
-                }
-
-                // tie(white, black, ch) = arr[idx];
-                // cout << white << ' ' << black << ' '<< ( ch == 'W' ? "White" : "Black" ) << '\n';
-            }
-
-            idx--;
+            offset = 1;
         }
 
-        // cout<< idx << '\n';
+        destination = idx + x[i] * offset;
+
+        for(; idx != destination; idx += offset)
+        {
+            if(arr.end() == arr.find(idx))
+            {
+                white = black = 0;
+                tmp = offset == 1 ? make_tuple(white,black+1, false) : make_tuple(white + 1,black, true);
+                arr.insert(make_pair(idx, tmp));
+            }
+            else
+            {
+                tie(white, black, isWhite) = arr[idx];
+                tmp = offset == 1 ? make_tuple(white,black+1, !isWhite) : make_tuple(white + 1,black, !isWhite);
+                arr[idx] = tmp;
+            }
+        }
+
+        idx -= offset;
     }
 
     white = black = gray = 0;
 
     int tmp_white, tmp_black;
-    char tmp_ch;
+    bool tmp_flag;
 
     for(auto result : arr)
     {
-
-        // cout << result.first << ' ';
-        tie(tmp_white, tmp_black,  tmp_ch) = result.second;
-        // cout << tmp_white << ' ' << tmp_black << ' '<< ( tmp_ch == 'W' ? "White" : "Black" ) << '\n';
-
+        tie(tmp_white, tmp_black, tmp_flag) = result.second;
         if(tmp_white >= 2 && tmp_black >= 2) gray++;
-        else if(tmp_ch == 'W') white++;
-        else if(tmp_ch == 'B') black++;
+        else if(tmp_flag) white++;
+        else if(!tmp_flag) black++;
     }
 
     cout << white << ' ' << black << ' ' << gray << '\n';
