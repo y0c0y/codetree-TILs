e5dcb312a13853f7cbdf99374a2c5dae5fde8e11
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-01-29 00:37:40 +0900

    Title: 흰검 칠하기, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260129/흰검 칠하기/painting-white-black.cpp
+++ 260129/흰검 칠하기/painting-white-black.cpp
@@ -34,23 +34,23 @@ int main() {
 
         destination = idx + x[i] * offset;
 
-        for(int j = idx; j != destination; j += offset)
+        for(; idx != destination; idx += offset)
         {
-            if(arr.end() == arr.find(j))
+            if(arr.end() == arr.find(idx))
             {
                 white = black = 0;
                 tmp = offset == 1 ? make_tuple(white,black+1, false) : make_tuple(white + 1,black, true);
-                arr.insert(make_pair(j, tmp));
+                arr.insert(make_pair(idx, tmp));
             }
             else
             {
-                tie(white, black, isWhite) = arr[j];
+                tie(white, black, isWhite) = arr[idx];
                 tmp = offset == 1 ? make_tuple(white,black+1, !isWhite) : make_tuple(white + 1,black, !isWhite);
-                arr[j] = tmp;
+                arr[idx] = tmp;
             }
         }
 
-        idx = destination - offset;
+        idx -= offset;
     }
 
     white = black = gray = 0;
@@ -61,15 +61,9 @@ int main() {
     for(auto result : arr)
     {
         tie(tmp_white, tmp_black, tmp_flag) = result.second;
-        if(tmp_white >= 2 &&  tmp_black >= 2) gray++;
-        else if(tmp_white >  tmp_black) white++;
-        else if(tmp_white <  tmp_black) black++;
-        else//tmp_white ==  tmp_black
-        {
-            if(tmp_flag) white++;
-            else black++;
-        }
-        
+        if(tmp_white >= 2 && tmp_black >= 2) gray++;
+        else if(tmp_flag) white++;
+        else black++;
     }
 
     cout << white << ' ' << black << ' ' << gray << '\n';
