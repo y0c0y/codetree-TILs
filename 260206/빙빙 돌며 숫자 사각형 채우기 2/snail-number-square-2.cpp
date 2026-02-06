1f032ff0fac4044ffa27122f80ed80ac8c828a86
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-02-06 20:13:30 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260206/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+int dy[4] = {1,0,-1,0};
+int dx[4] = {0,1,0,-1};
+
+
+
+
+bool InRange(int y, int x)
+{
+    return 0<= y && y < n && 0<=x && x < m;
+}
+
+int main() {
+    cin >> n >> m;
+    
+    int arr[n][m] = {};
+    
+    int y = 0;
+    int x = 0;
+    int dir = 0;
+    int Y = 0;
+    int X = 0;
+
+    for(int cnt = 1; cnt <= n * m ; cnt++)
+    {       
+        arr[y][x] = cnt;
+        
+        Y = y + dy[dir];
+        X = x + dx[dir];
+        if(!InRange(Y, X) || arr[Y][X] != 0)
+        {
+            dir = (dir+1)%4;
+            Y = y + dy[dir];
+            X = x + dx[dir];  
+        }
+        
+        y = Y;
+        x = X;       
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+
+
+    return 0;
+}
