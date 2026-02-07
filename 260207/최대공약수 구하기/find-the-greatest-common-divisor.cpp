d3f0e03f31f10009438e056f28b6e58b497b177a
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-02-07 23:41:25 +0900

    Title: 최대공약수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260207/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    cin >> n >> m;
+
+    cout << gcd(n,m);
+
+    // Please write your code here.
+
+    return 0;
+}
