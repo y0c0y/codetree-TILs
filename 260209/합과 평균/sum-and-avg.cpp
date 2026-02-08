af840844e0e67cbb61e80962b9b5b5645769ec62
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-02-09 00:02:54 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260209/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a,b;
+    cin >> a >> b;
+    cout << (a+b) << ' ';
+    cout << floor((a+b)*10.0/20);
+    return 0;
+}
