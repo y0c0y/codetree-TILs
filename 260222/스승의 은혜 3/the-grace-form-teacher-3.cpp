0df1e2969730840af8ea6969f09948d1009c664e
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-02-22 22:23:41 +0900

    Title: 스승의 은혜 3, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260222/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 260222/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -6,57 +6,43 @@ int N, B;
 int P[1000];
 int S[1000];
 
-int fun(int total, int i, bool useCoupon, int cnt)
-{
-    // cout << i << ' ' << total << '\n';
-
-    if(i >= N || total > B) return cnt - 1;
-    if(total == B) return cnt;
-
-    int price = P[i];
-    int shipping = S[i];
-
-    int count[3] = {};
-
-    if(total + price + shipping <= B)
-    {
-        count[0] = fun(total + price + shipping, i++, useCoupon, cnt + 1);
-    }
-    else if(total + price / 2 + shipping <= B && !useCoupon)
-    {
-        count[1] = fun(total + price / 2 + shipping, i++, true, cnt + 1);
-    }
-    else
-    {
-       count[2] = fun(total, i++, useCoupon, cnt);
-    }
-
-    int result = *max_element(count, count + 3);
-
-    return result;
-}
 
 int main() {
     cin >> N >> B;
 
-    vector<pair<int, int>> students;
-    pair<int, int> tmp;
-
-    int maxIdx = INT_MIN;
-
-    int total = 0;
+    int maxVal = INT_MIN;
 
     for (int i = 0; i < N; i++) {
         cin >> P[i] >> S[i];
     }
 
-    // for (int i = 0; i < N; i++) {
-    //         for (int i = 0; i < N; i++) {
-    //         cin >> P[i] >> S[i];
-    //     }
-    // }
+    int total = 0;
+    int cnt = 0;
 
-    cout << fun(0, 0, false, 0);
+    for (int i = 0; i < N; i++) {
+        total = 0;
+        cnt = 0;
+
+        total += P[i]/2 + S[i];
+
+        if(total <= B)
+        {
+            cnt++;
+            for (int j = 0; j < N; j++) {
+                if(i == j) continue;
+                total += P[j] + S[j];
+                if(total <= B) cnt++;
+                else break;
+            }
+        }
+
+        maxVal = max(maxVal, cnt);
+
+
+        
+    }
+
+    cout <<  maxVal;
 
 
    
