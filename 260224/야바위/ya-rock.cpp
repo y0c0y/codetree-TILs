13ad546c2e4f62f00adda7150e0b08c5bb5befc1
조윤희 <68114427+y0c0y@users.noreply.github.com>
2026-02-24 22:15:28 +0900

    Title: 야바위, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260224/야바위/ya-rock.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+int a[100], b[100], c[100];
+
+int main() {
+    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> a[i] >> b[i] >> c[i];
+    }
+
+    int maxScore = 0;
+    int score = 0;
+    int idx = 0;
+
+    for(int j = 0 ; j <3 ; j++)
+    {   
+        int info[3] = {0,0,0};
+
+        info[j] = 1;
+
+        score = 0;
+
+        for (int i = 0; i < N; i++) 
+        {
+           swap(info[a[i] - 1],info[b[i] - 1]);
+            if(info[c[i] - 1]) score++;
+        }
+
+       if(score >= maxScore)
+       {
+            maxScore = score;
+            idx = j;
+       }
+    }
+
+    cout << idx + 1;
+
+    // Please write your code here.
+
+
+
+    return 0;
+}
